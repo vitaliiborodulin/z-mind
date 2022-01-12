ebe974968cb08432b200cf2d6ca8ec763eafbfbb
Vitalii Borodulin <vitaliiborodulin@yandex.ru>
2022-01-12 18:27:02 +0300

    updates and fixes

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -1,62 +1,62 @@
-$('#course .course__list').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    // centerMode: true,
-    // centerPadding: '40px',
-    // autoplay: true,
-    // autoplaySpeed: 3000,
-    responsive: [
-        {
-            breakpoint: 992,
-            settings: {
-                slidesToShow: 2
-            }
-        },
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 1,
-                // centerMode: true,
-                // centerPadding: '10px',
-            }
-        },
-    ]
-});
+if ($('body').hasClass('home')) {
+    $('#course .course__list').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        // centerMode: true,
+        // centerPadding: '40px',
+        // autoplay: true,
+        // autoplaySpeed: 3000,
+        responsive: [{
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    // centerMode: true,
+                    // centerPadding: '10px',
+                }
+            },
+        ]
+    });
 
-$('.team__slider').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    centerMode: true,
-    centerPadding: '40px',
-    autoplay: true,
-    autoplaySpeed: 2000,
-    responsive: [
-        {
-            breakpoint: 1200,
-            settings: {
-                slidesToShow: 2
+    $('.team__slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '40px',
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [{
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    centerMode: true,
+                    centerPadding: '20px',
+                }
             }
-        },
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 1,
-                centerMode: true,
-                centerPadding: '20px',
-            }
-        }
-    ]
-});
+        ]
+    });
 
-$('.review__slider').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    centerMode: true,
-    centerPadding: '20px',
-    autoplay: true,
-    autoplaySpeed: 3000,
-    // adaptiveHeight: true
-});
+    $('.review__slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '20px',
+        autoplay: true,
+        autoplaySpeed: 3000,
+        // adaptiveHeight: true
+    });
+}
